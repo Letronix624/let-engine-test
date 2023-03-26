eeb7434b11b4ef21e58bb28ef57bc04e427de887
Let <94926994+Letronix624@users.noreply.github.com>
2023-03-26 14:54:20 +0200

    added my font

--- src/main.rs
+++ src/main.rs
@@ -33,78 +33,35 @@ fn main() {
         .with_app_info(app_info)
         .build();
 
-
-    // let image_bytes = include_bytes!("../assets/textures/placeholder.png");
-    // let image_cursor = Cursor::new(image_bytes);
-
-    // let decoder = png::PngDecoder::new(image_cursor).unwrap();
-    // let dimensions = decoder.dimensions();
-    // let mut buf = vec![0; (dimensions.0 * dimensions.1) as usize * 4];
-    // decoder.read_image(&mut buf).unwrap();
-
-    // game.load_texture("placeholder", buf, dimensions.0, dimensions.1);
-
-    // game.load_font_bytes(
-    //     "Rawr-Regular",
-    //     include_bytes!("../assets/fonts/Rawr-Regular.ttf"),
-    // );
-
-    // let background = Arc::new(Mutex::new(Object::new_square()));
-    // {
-    //     let mut background = background.lock();
-    //     background.position = [0.0, 1.0];
-    //     background.size = [1.0; 2];
-    //     background.graphics = Some(
-    //         Appearance::new_square().texture("placeholder").material(1)
-    //     );
-    // }
-
-    // game.add_object(&background);
-
-    // let mut label = Object::new_square();
-    // label.size = [1.0; 2];
-    // label.position = [0.0, 0.5];
-    // label.graphics = Some(
-    //     game.get_font_data(
-    //         "Rawr-Regular",
-    //         "Test test nice nice",
-    //         70.0,
-    //         [1.0; 4]
-    //     )
-    // );
-
-    // game.add_child_object(&background, &Arc::new(Mutex::new(label)));
-
-    //let bg = background.lock().unwrap();
-
-    //bg.size;
+    game.load_font_bytes(
+        "Rawr-Regular",
+        include_bytes!("../assets/fonts/Hand-Regular.ttf"),
+    );
 
     let layer = game.new_layer();
 
-    let mut someobj = Object::new_square();
+    // Objects
+    let mut text = Object::new();
 
-    someobj.camera = Some(
-        CameraOption {
-            zoom: 1.5,
-            mode: CameraScaling::Expand
-        }
+    text.graphics = game.get_font_data(
+        "Rawr-Regular",
+        "ABC abc Super Cool!!",
+        70.0,
+        [1.0; 4]
     );
-    someobj.position = [1.0, 0.0];
+    let mut text2 = text.clone();
+    text2.position = [0.0, 0.3];
 
-    let mut other = Object::new();
+    let _text = game.add_object(&layer, text).unwrap();
+    let _text2 = game.add_object(&layer, text2).unwrap();
 
-    other.size = [0.1; 2];
-    other.graphics = Some(
-        Appearance::new()
-            .data(make_circle!(10))
-            .color([0.1, 0.0, 1.0, 1.0])
-    );
+    let camera = game.add_object(&layer, Object::new()).unwrap();
 
-    let someobj = game.add_object(&layer, someobj).unwrap();
+    camera.lock().camera = Some( CameraOption {zoom: 1.0, mode: CameraScaling::Limited} );
 
-    let other = game.add_object(&layer, other).unwrap();
+    game.set_camera(&layer, &camera).unwrap();
 
-    game.set_camera(&layer, &someobj).unwrap();
+    
     
     let mut mist = false;
 
@@ -147,6 +104,7 @@ fn main() {
             }
             Event::RedrawEventsCleared => {
                 game.update();
+                println!("{}", game.fps());
                 // if mist
                 // {
                 //     println!("{}", game.contains_object(&background));   
@@ -157,16 +115,6 @@ fn main() {
                 // }
                 // println!("{}", 1.0 / dt.elapsed().as_secs_f64());
 
-                {
-                    let mut big = someobj.lock();
-                    // other.position = [
-                    //     secs.cos(),
-                    //     secs.sin(),
-                    // ];
-
-                    big.rotation = game.time() as f32;
-
-                }
             }
             _ => (),
         }

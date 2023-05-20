5db04c1b14e650c8c61d95dd798ed5bc65005e6a
Letronix624 <timbukreew624@gmail.com>
2023-05-20 18:44:18 +0200

    0.5.0

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ use winit::event::VirtualKeyCode;
 use winit::event::WindowEvent;
 use winit::window::WindowBuilder;
 
+#[object]
+#[derive(Default)]
+struct Object {}
+
+#[camera_object]
+#[derive(Default)]
+struct Camera {}
+
 fn main() {
     let window_builder = WindowBuilder::new()
         .with_resizable(true)
@@ -27,49 +35,79 @@ fn main() {
     let layer = game.scene.new_layer();
     let mut txt = String::new();
     let fsize = 35.0;
-    let rtext = Object::new().graphics(Some(
-        Appearance::new_color([1.0, 0.0, 0.0, 1.0]).size(vec2(2.6, 2.6)),
-    ));
-    let gtext = Object::new().graphics(Some(
-        Appearance::new_color([0.0, 1.0, 0.0, 1.0]).size(vec2(2.6, 2.6)),
-    ));
-    let btext = Object::new().graphics(Some(
-        Appearance::new_color([0.0, 0.0, 1.0, 1.0]).size(vec2(2.6, 2.6)),
-    ));
-    let rtext = layer.add_object(None, rtext).unwrap();
-    let gtext = layer.add_object(None, gtext).unwrap();
-    let btext = layer.add_object(None, btext).unwrap();
+    let mut rtext = Label::new(
+        &resources,
+        &font,
+        LabelCreateInfo {
+            appearance: Appearance::new_color([1.0, 0.0, 0.0, 1.0]).transform(Transform::default().size(vec2(2.0, 2.0))),
+            text: txt.clone(),
+            scale: vec2(fsize, fsize),
+            align: NW,
+            ..Default::default()
+        }
+    );
+    let mut gtext = Label::new(
+        &resources,
+        &font,
+        LabelCreateInfo {
+            appearance: Appearance::new_color([0.0, 1.0, 0.0, 1.0]).transform(Transform::default().size(vec2(2.0, 2.0))),
+            text: txt.clone(),
+            scale: vec2(fsize, fsize),
+            align: CENTER,
+            ..Default::default()
+        }
+    );
+    let mut btext = Label::new(
+        &resources,
+        &font,
+        LabelCreateInfo {
+            appearance: Appearance::new_color([0.0, 0.0, 1.0, 1.0]).transform(Transform::default().size(vec2(2.0, 2.0))),
+            text: txt.clone(),
+            scale: vec2(fsize, fsize),
+            align: SO,
+            ..Default::default()
+        }
+    );
+  //let rtext = Object::new().graphics(Some(
+  //    Appearance::new_color([1.0, 0.0, 0.0, 1.0]).size(vec2(2.6, 2.6)),
+  //));
+  //let gtext = Object::new().graphics(Some(
+  //    Appearance::new_color([0.0, 1.0, 0.0, 1.0]).size(vec2(2.6, 2.6)),
+  //));
+  //let btext = Object::new().graphics(Some(
+  //    Appearance::new_color([0.0, 0.0, 1.0, 1.0]).size(vec2(2.6, 2.6)),
+  //));
+    layer.add_object(None, &mut rtext).unwrap();
+    layer.add_object(None, &mut gtext).unwrap();
+    layer.add_object(None, &mut btext).unwrap();
 
-    resources.queue_to_label(&rtext, &font, &txt, fsize, NW);
-    resources.queue_to_label(&gtext, &font, &txt, fsize, CENTER);
-    resources.queue_to_label(&btext, &font, &txt, fsize, SO);
-    let mut camera = Object::new();
-    camera.position = vec2(0.0, 0.0);
-    let camera = layer.add_object(None, camera).unwrap();
-    camera.lock().camera = Some(CameraOption {
-        zoom: 1.0,
-        mode: CameraScaling::Expand,
-    });
+  //resources.queue_to_label(&rtext, &font, &txt, fsize, NW);
+  //resources.queue_to_label(&gtext, &font, &txt, fsize, CENTER);
+  //resources.queue_to_label(&btext, &font, &txt, fsize, SO);
+    let mut camera = Camera::default();
+    camera.camera.mode = CameraScaling::Expand;
+    layer.add_object(None, &mut camera).unwrap();
     game.set_clear_background_color([0.3, 0.0, 0.3, 0.1]);
-    layer.set_camera(&camera).unwrap();
+    layer.set_camera(camera.clone());
     resources.get_window().set_visible(true);
 
-    let mut test = Object::new().graphics(Some(
-        Appearance::new().size(vec2(0.1, 0.1)).data(make_circle!(10, 0.5)),
-    ));
+    let mut test = Object::default();
+    test.appearance = Appearance::new().data(make_circle!(10, 0.5));
+    test.appearance.transform.size = vec2(0.1, 0.1);
+    
 
-    test.position = layer.side_to_world(N, (1000.0, 700.0));
-    let _test = layer.add_object(None, test).unwrap();
+    test.transform.position = layer.side_to_world(N, (1000.0, 700.0));
+    layer.add_object(None, &mut test).unwrap();
 
     let mut last = false;
     let mut right = false;
     let mut mouselock = vec2(0.0, 0.0);
     let mut camera_lock = vec2(0.0, 0.0);
+    let mut egui_focused = false;
 
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_poll();
-        game.update(&event);
-        match event {
+        match &event {
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::CloseRequested => control_flow.set_exit(),
                 WindowEvent::KeyboardInput { input, .. } => {
@@ -82,25 +120,25 @@ fn main() {
                         '\u{8}' => {
                             txt.pop();
                         }
-                        _ if c != '\u{7f}' => txt.push(c),
+                        _ if *c != '\u{7f}' => txt.push(*c),
                         _ => {}
                     }
-                    resources.queue_to_label(&rtext, &font, &txt, fsize, NW);
-                    resources.queue_to_label(&gtext, &font, &txt, fsize, CENTER);
-                    resources.queue_to_label(&btext, &font, &txt, fsize, SO);
+                    rtext.update_text(txt.clone());
+                  //resources.queue_to_label(&rtext, &font, &txt, fsize, NW);
+                    gtext.update_text(txt.clone());
+                  //resources.queue_to_label(&gtext, &font, &txt, fsize, CENTER);
+                    btext.update_text(txt.clone());
+                  //resources.queue_to_label(&btext, &font, &txt, fsize, SO);
                 }
                 WindowEvent::MouseWheel { delta, .. } => {
                     if let MouseScrollDelta::LineDelta(_, y) = delta {
-                        let mut camera = camera.lock();
-                        let zoom = camera.camera.unwrap().zoom;
-                        camera.camera.as_mut().unwrap().zoom +=
-                            y as f32 * zoom * 0.1;
+                        let zoom = camera.camera.zoom;
+                        camera.camera.zoom = zoom + *y as f32 * zoom * 0.1;
                     };
                 }
                 _ => (),
             },
             Event::MainEventsCleared => {
-
                 #[cfg(debug_assertions)]
                 game.update_gui(|ctx| {
                     egui::TopBottomPanel::top("test").show(&ctx, |ui| {
@@ -109,16 +147,18 @@ fn main() {
                             control_flow.set_exit();
                         }
                     });
+                    egui_focused = ctx.is_pointer_over_area();
                 });
-
+                if egui_focused {
+                    return;
+                }
                 {
                     if input.mouse_down(&MouseButton::Left) && !last {
-                        let mut object = Object::new()
-                            .graphics(Some(Appearance::new().size(vec2(0.03, 0.03)).data(Data::square())));
-                        object.position = input.cursor_to_world(&layer);
-                        {
-                            layer.add_object(None, object).unwrap();
-                        }
+                        let mut object = Object::default();
+                        object.appearance = Appearance::new().data(Data::square());
+                        object.appearance.transform.size = vec2(0.03, 0.03);
+                        object.transform.position = input.cursor_to_world(&layer);
+                        layer.add_object(None, &mut object).unwrap();
                     }
                     last = input.mouse_down(&MouseButton::Left);
                 }
@@ -126,7 +166,7 @@ fn main() {
                     let cp = input.scaled_cursor(&layer);
                     if input.mouse_down(&MouseButton::Middle) && !right {
                         mouselock = cp;
-                        camera_lock = camera.lock().position;
+                        camera_lock = camera.transform().position;
                     }
                     if input.mouse_down(&MouseButton::Middle) {
                         let shift = vec2(
@@ -136,12 +176,14 @@ fn main() {
                                 + camera_lock[1],
                         );
                         //times camera mode please
-                        camera.lock().position = shift;
+                        camera.transform.position = shift;
                     }
                     right = input.mouse_down(&MouseButton::Middle);
                 }
+                layer.set_camera(camera.clone());
             }
             _ => (),
         }
+        game.update(&event);
     });
 }
